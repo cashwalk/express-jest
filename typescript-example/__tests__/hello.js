8a232ec2835637b222b543769e3ab6a693c3ce8e
Park Hyeojun <hj.park@cashwalk.io>
2021-03-15 11:29:36 +0900

    잘못 컴파일된 파일들 삭제

--- typescript-example/__tests__/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../src/app");
-test('null', function () {
-    var n = null;
-    console.log(123);
-    expect(n).toBeNull();
-});
-//# sourceMappingURL=hello.js.map
