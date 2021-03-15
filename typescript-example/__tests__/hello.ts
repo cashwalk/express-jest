3e665b4c203777ac7dea260e013abe269f4c5f8f
Park Hyeojun <hj.park@cashwalk.io>
2021-03-15 12:08:02 +0900

    Test 환경에서 src 로 접근하기 위한 alias 설정

--- typescript-example/__tests__/hello.ts
+++ typescript-example/__tests__/hello.ts
@@ -1,6 +1,17 @@
+import {app} from "@/app"
+import {app as user} from "@/services/manage_user"
 
 test('null', () => {
   const n = null;
   console.log(123)
   expect(n).toBeNull();
 });
+
+//Test to import file by alias path
+it('mount app', () => {
+  console.log(app)
+})
+
+it('mount service', () => {
+  console.log(user)
+})
--- typescript-example/jest.config.js
+++ typescript-example/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
 };
