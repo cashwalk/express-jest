92ce198331799c55e53434214c3ed6f909022504
Park Hyeojun <hj.park@cashwalk.io>
2021-03-25 16:13:33 +0900

    Squashed commit of the following:
    commit 93f2a6d8d46c3a9c56e81d96b7653ba68f3fff83
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 16:11:08 2021 +0900
    
        CI/CD script for javascript is updated
    
    commit 79fbb5b85a99bafb5c0a55a43c739e26fa328862
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 16:03:44 2021 +0900
    
        DynamoDB 사용 typescript 과 동기화
    
    commit fc7a6d7780073b62e39bf0df1a3f4a5aee053b79
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 15:42:56 2021 +0900
    
        에러가 나지 말아야 할 부분 에러핸들링 블럭 밖으로 이동
    
    commit 233fe888d7d1bf66c500d5b4b19a6b4e483380dd
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 13:58:18 2021 +0900
    
        env is changed
    
    commit 9cbcdb5ce56c212aee5eac27159684d790fa8b95
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 13:51:06 2021 +0900
    
        DynamoDB 연동
    
    commit de96a4f6b3ea277c30e4a75ce7f04dab64f580a0
    Author: Park Hyeojun <hj.park@cashwalk.io>
    Date:   Thu Mar 25 13:50:40 2021 +0900
    
        변수명 변경

---
+++ typescript-example/src/repositories/dynamoImpl/UserRepoDynamo.ts
@@ -0,0 +1,121 @@
+import {User} from "../../domains/users";
+import UserRepo from "../UserRepo";
+import {DocumentClient} from "aws-sdk/clients/dynamodb";
+
+const AWS = require('aws-sdk');
+
+const TABLE_NAME = 'Users';
+const AWS_REGION = process.env.AWS_REGION || 'local';
+const AWS_ENDPOINT = 'http://' + (process.env.AWS_ENDPOINT || 'localhost:8000');
+const AWS_API_VERSION = process.env.AWS_API_VERSION || '2012-08-10';
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || 'demo';
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || 'demo';
+
+AWS.config.update({
+  region: AWS_REGION,
+  endpoint: AWS_ENDPOINT,
+  apiVersion: AWS_API_VERSION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+});
+
+export default class UserRepoDynamo implements UserRepo{
+  private client;
+  
+  constructor() {
+    this.client = new DocumentClient();
+  }
+  
+  async insertUser(user: User): Promise<User> {
+    let param = this.wrapUserAsParamToUpdate(user);
+    const req = new Promise<User> (async (resolve, reject) => {
+      await this.checkDuplicate(user.username, reject);
+      this.client.put(param, async (err, data) => {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+          reject(err);
+        }
+        
+        let inserted = await this.findUserByUsername(user.username);
+        resolve(inserted);
+      });
+    });
+    return req;
+  }
+  
+  async findUserByUsername(username: string): Promise<User> {
+    let params = this.wrapUserAsParamToGet({ username }, false);
+    return new Promise<User> (  (resolve, reject) => {
+        this.client.query(params, (err, data) => {
+          if (err) {
+            console.error("Unable to get item. Error JSON:", JSON.stringify(err, null, 2));
+            reject(err);
+          }
+          
+          resolve(data.Items.pop());
+        });
+      }
+    );
+  }
+  
+  async truncate() {
+    const scanParams = {
+      TableName: TABLE_NAME,
+    }
+    const prosedure = new Promise( (resolve, reject) => {
+      this.client.scan(scanParams, (scanErr, scanData) => {
+        const delItem = (rw) => {
+          const delParams = {
+            TableName: TABLE_NAME,
+            Key: {
+              username: rw.username,
+              id: rw.id
+            },
+          }
+          this.client.delete(delParams, (err, data) => {
+            if (err) {
+              console.error("Unable to get item. Error JSON:", JSON.stringify(err, null, 2));
+              reject(err);
+            }
+          
+            resolve(data);
+          });
+        };
+        if (scanData.Items.length == 0) {
+          resolve(true);
+        }
+        scanData.Items.forEach(delItem);
+      })
+    });
+    await prosedure;
+  }
+  
+  private async checkDuplicate(username: string, reject: (string)=>void) {
+    let preRow = await this.findUserByUsername(username);
+    if(preRow) {
+      reject('Duplicate username');
+    }
+  }
+  private wrapUserAsParamToUpdate(user: User, isNew: boolean = true) {
+    user.id = isNew ?  this.fakeUUID(user.username) : user.id;
+    return {
+      TableName: TABLE_NAME,
+      Item: user,
+    };
+  }
+  
+  private wrapUserAsParamToGet(user: User, ...opts) {
+    return {
+      TableName: TABLE_NAME,
+      KeyConditionExpression: "username = :username",
+      ExpressionAttributeValues: {
+        ":username": user.username
+      },
+      ...opts
+    };
+  }
+  
+  private fakeUUID (name: string): string {
+    return new Date().toTimeString() + '-' + name;
+  }
+}
